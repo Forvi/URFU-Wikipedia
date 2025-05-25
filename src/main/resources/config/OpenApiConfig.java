ac75f33aa3f4f4fd4574e1b0a7400b38c4f2534c
tetyukovilya <tetyukov06@gmail.com>
2025-05-25 20:22:09 +0500

    Preparing for configuration with swagger

---
+++ src/main/resources/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package org.app.lifemarchforecastingbackend.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+/**
+ * Класс конфигурации Swagger.
+ * Swagger - инструмент для документирования и
+ * тестирования API.
+ *
+ * @info - информация о API
+ * @title - заголовок
+ * @description - описание
+ * @version - версия
+ *
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Lifemarch forecasting application - backend",
+                description = "API приложения для прогнозирования выручки магазина Жизньмарт",
+                version = "1.0.0"
+        )
+)
+public class OpenApiConfig {
+    // Swagger Config
+}
